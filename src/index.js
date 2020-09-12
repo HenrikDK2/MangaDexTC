54692d28e3093bd023cd4e0ffe18068725a52468
HenrikDK2 <henrik265@outlook.dk>
2020-09-12 11:44:39 +0200

    Lang prompt

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { getChapterArr, addTotalChaptersToDom } from "./util.js";
 (async () => {
   const mangas = document.querySelectorAll(".manga-entry");
   const apiUrl = "https://mangadex.org/api/manga/";
-  const lang = "English";
+  const lang = !localStorage.getItem("lang")
+    ? prompt("Chapters language?", "English")
+    : localStorage.getItem("lang");
+  localStorage.setItem("lang", lang);
 
   //Create Stylesheet for Class
   let style = document.createElement("style");
-  style.innerHTML =
-    ".totalChapterElement { box-shadow: 0 0 2px 0 rgba(0,0,0,.4); color: red; font-weight:bold; padding: 0 .5rem; }";
+  style.innerHTML = `.totalChapterElement { margin-top: .2rem; box-shadow: 0 0 2px 0 rgba(0,0,0,.4); color: red; font-weight:bold; padding: 0 .5rem; }`;
   document.head.appendChild(style);
 
   for (let manga of mangas) {
