1b6ce2530e22c2559a6d0cf0b9540300d6d0c63d
HenrikDK2 <henrik265@outlook.dk>
2020-09-12 11:27:29 +0200

    Seperated functions from Index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-const mangas = document.querySelectorAll(".manga-entry");
-const apiUrl = "https://mangadex.org/api/manga/";
-const lang = "English";
+import { getChapterArr, addTotalChaptersToDom } from "./util.js";
+
 (async () => {
+  const mangas = document.querySelectorAll(".manga-entry");
+  const apiUrl = "https://mangadex.org/api/manga/";
+  const lang = "English";
+
   //Create Stylesheet for Class
   let style = document.createElement("style");
   style.innerHTML =
@@ -11,33 +14,7 @@ const lang = "English";
   for (let manga of mangas) {
     const id = manga.getAttribute("data-id");
     const data = await fetch(apiUrl + id).then((res) => res.json());
-    let chapterArr = getChapterArr(data.chapter);
+    let chapterArr = getChapterArr(data.chapter, lang);
     addTotalChaptersToDom(manga, chapterArr);
   }
 })();
-
-function addTotalChaptersToDom(manga, chapterArr) {
-  //Add chapterCount into DOM
-  if (!manga.querySelector(".totalChapterElement")) {
-    let chapterCountElm = document.createElement("li");
-    chapterCountElm.classList.add("list-inline-item", "totalChapterElement");
-    chapterCountElm.innerHTML = `<span>${chapterArr.length}</span>`;
-    manga.querySelector(".list-inline").append(chapterCountElm);
-  } else {
-    chapterCountElm.innerHTML = `<span>${chapterArr.length}</span>`;
-  }
-}
-
-function getChapterArr(chapterObj) {
-  let mySet = new Set();
-
-  //Filter by Language and removes Chapter duplicates.
-  return Object.keys(chapterObj).filter((id) => {
-    if (chapterObj[id]["lang_name"] === lang) {
-      let key = chapterObj[id].chapter,
-        isNew = !mySet.has(key);
-      if (isNew) mySet.add(key);
-      return isNew;
-    }
-  });
-}
---
+++ src/util.js
@@ -0,0 +1,25 @@
+//Filter by Language and removes Chapter duplicates.
+export function getChapterArr(chapterObj, lang) {
+  let mySet = new Set();
+
+  return Object.keys(chapterObj).filter((id) => {
+    if (chapterObj[id]["lang_name"] === lang) {
+      let key = chapterObj[id].chapter,
+        isNew = !mySet.has(key);
+      if (isNew) mySet.add(key);
+      return isNew;
+    }
+  });
+}
+
+//Add chapterCount into DOM
+export function addTotalChaptersToDom(manga, chapterArr) {
+  if (!manga.querySelector(".totalChapterElement")) {
+    let chapterCountElm = document.createElement("li");
+    chapterCountElm.classList.add("list-inline-item", "totalChapterElement");
+    chapterCountElm.innerHTML = `<span>${chapterArr.length}</span>`;
+    manga.querySelector(".list-inline").append(chapterCountElm);
+  } else {
+    chapterCountElm.innerHTML = `<span>${chapterArr.length}</span>`;
+  }
+}
